b903af9d171346f9484ae94ee77b97113405c6b3
ClemDoum <clement.doumouro@gmail.com>
2018-04-04 11:34:11 +0200

    Fix CLI

--- cli/cli.py
+++ cli/cli.py
@@ -5,7 +5,9 @@ import io
 import json
 import os
 import sys
+
 from builtins import bytes, input
+
 
 from snips_nlu import SnipsNLUEngine, NLUEngineConfig, load_resources
 
@@ -137,7 +139,7 @@ def parse_train_test_args(args):
     parser.add_argument("test_dataset_path", type=str)
     parser.add_argument("output_path", type=str)
     parser.add_argument(
-        "-esm", "--exclude-slot-metrics", action="store_true",
+        "-s", "--exclude-slot-metrics", action="store_true",
         help="Exclude slot metrics and slot errors in the output")
     parser.add_argument("-i", "--include-errors", action="store_true",
                         help="Include parsing errors in the output")
